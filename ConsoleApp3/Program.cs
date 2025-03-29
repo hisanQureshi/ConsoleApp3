a49ea6ceb737c19d61f35f20cca07145e9bc08dd
PMYLS <PMYLS@DESKTOP-8BL3MIG>
2025-03-29 13:21:38 +0500

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fname= "John";
+            string lname = "Doe";
+            string fullname = fname + " " + lname;
+            Console.WriteLine("Hello, " + fullname);
+            Console.WriteLine(fullname.Length);
+            Console.WriteLine(fullname.ToUpper());
+            Console.WriteLine(fullname.ToLower());
+            Console.WriteLine(fullname.Contains("John"));
+            Console.WriteLine(fullname.Contains("john"));
+            Console.WriteLine(fullname.Contains("Doe"));
+            Console.WriteLine(fullname.Contains("doe"));
+            Console.WriteLine(fullname.Contains("Jane"));
+
+        }
+    }
+}
